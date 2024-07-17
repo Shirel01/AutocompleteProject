fff9226724087c494e79a8faa41996d15b29409d
Shirel01 <shirel.israel2001@gmail.com>
2024-07-17 14:14:22 +0300

    add sentenceTrie and wordTrie

---
+++ sentence_trie.py
@@ -0,0 +1,37 @@
+
+
+class WordNode:
+    def __init__(self, word, father, children=None):
+        self.word = word
+        self.father = father
+        self.children = children or []
+
+
+class SentenceTrie:
+    def __init__(self):
+        self.root = WordNode(None, None)
+
+    def add_sentence(self, sentence: str) -> None:
+        """
+        Adds a sentence to the trie
+        :param sentence: str
+        :return: None
+        """
+        words = sentence.split()
+        node = self.root
+        for word in words:
+            node = self.add_word(word, node)
+
+    def add_word(self, word: str, father: WordNode) -> WordNode:
+        """
+        Adds a word to the trie, called only by add_sentence
+        :param word: the current word in the sentence
+        :param father: the father node of the current word
+        :return: the node of the current word
+        """
+        for child in father.children:
+            if child.word == word:
+                return child
+        node = WordNode(word, father) #create a new node with the word that we have added
+        father.children.append(node) #add this children to the father
+        return node
---
+++ word_trie.py
@@ -0,0 +1,46 @@
+import sentence_trie
+from sentence_trie import WordNode
+
+
+class TrieNode:
+    """A node in the trie structure"""
+
+    def __init__(self, char):
+        self.char = char
+        self.is_end_of_word = False
+        # A counter indicating how many times a word is inserted
+        # (if this node's is_end is True)
+        self.counter = 0
+        # A dictionary of child nodes
+        # Keys are characters, values are nodes
+        self.children = {}
+        # Array of references of words in SentenceTrie
+        self.sentenceTrieRef = []
+
+
+class WordTrie:
+
+    def __init__(self):
+        self.root = TrieNode("")
+
+    def insert(self, word: WordNode):
+        """Insert a word into the trie"""
+        node = self.root
+
+        # Loop through each character in the word
+        # Check if there is no child containing the character, create a new child for the current node
+        for char in sentence_trie.WordNode.word:
+            if char in node.children:
+                node = node.children[char]
+            else:
+                newNode = TrieNode(char)
+                node.children[char] = newNode
+                node = newNode
+
+        node.is_end_of_word = True
+        # Increment the counter to indicate that we see this word once more
+        node.counter += 1
+        node.sentenceTrieRef.append(word)
+
+
+
